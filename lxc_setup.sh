60f86b3d65f2995876312c9761cc086ea38ac4a4
Andrew Wang <pkid0anpig@gmail.com>
2022-01-25 14:47:14 +0800

    delete dotfiles

--- lxc_setup.sh
+++ lxc_setup.sh
@@ -1,21 +1,30 @@
 #! /bin/sh
-if [ $# -lt 2 ]; then
-    exit 1
-fi
 if [ "$(whoami)" != "root" ]; then
+    echo Please run with root privileges.
     exit 1
 fi
-apt update -y;
-apt upgrade -y;
-apt install vim tmux wireguard zsh -y;
-useradd anpig -s /usr/bin/zsh -m;
-touch /home/anpig/.zshrc;
-echo "$1":"$2" | chpasswd;
-su - "$1";
-cd /home/"$1" || exit
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
-mv "$(dirname "$0")"/dotfiles/.p10k.zsh /home/"$1";
-mv "$(dirname "$0")"dotfiles/.zshrc /home/"$1";
-exit;
-exit
+if [ $# -eq 2 ]; then
+    apt update -y;
+    apt upgrade -y;
+    apt install vim tmux wireguard git -y;
+    useradd anpig -m;
+    echo "$1":"$2" | chpasswd;
+    su - "$1" -c '
+        cd || exit
+        sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+        git clone https://github.com/anpig/dotfiles.git
+        rm -rf dotfile/.git
+        cp -a dotfiles/. .
+        rm -rf dotfiles
+        rm -rf .git
+        rm .profile
+        rm .bashrc
+        rm .bash_profile
+        exit
+    '
+    apt install zsh -y
+else
+    echo Usage: ./lxc_setup username password
+    exit 1
+fi
